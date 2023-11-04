5062ebfdefeca66b4faa51dcffcb54e79b9d188a
AtefMMO <mohamedatefimb@gmail.com>
2023-11-04 12:54:23 +0200

    settings tap and localization for app text done

--- lib/home/localization_bottomsheet.dart
+++ lib/home/localization_bottomsheet.dart
@@ -1,16 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-class SettingsBottomSheet extends StatelessWidget {
+import 'package:provider/provider.dart';
+
+import '../providers/app_config.dart';
+
+class SettingsBottomSheet extends StatefulWidget {
+  @override
+  State<SettingsBottomSheet> createState() => _SettingsBottomSheetState();
+}
+
+class _SettingsBottomSheetState extends State<SettingsBottomSheet> {
+  late AppConfigProvider provider;
   @override
   Widget build(BuildContext context) {
+    provider = Provider.of<AppConfigProvider>(context);
     return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
+        padding: const EdgeInsets.all(8.0),
+        child: provider.language == 'en'
+            ? bottomSheet(context, 'en')
+            : bottomSheet(context, 'ar'));
+  }
+
+  Widget bottomSheet(BuildContext context, String language) {
+    if (provider.language == 'en') {
+      return Column(
         children: [
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: InkWell(
-              onTap: () {},
+              onTap: () {
+                provider.changeLang('en');
+              },
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -30,7 +50,9 @@ class SettingsBottomSheet extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: InkWell(
-              onTap: () {},
+              onTap: () {
+                provider.changeLang('ar');
+              },
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -47,7 +69,55 @@ class SettingsBottomSheet extends StatelessWidget {
             ),
           )
         ],
-      ),
-    );
+      );
+    } else {
+      return Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: InkWell(
+              onTap: () {
+                provider.changeLang('en');
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    AppLocalizations.of(context)!.english,
+                    style: TextStyle(fontSize: 30),
+                  ),
+                  Icon(
+                    Icons.check,
+                    size: 30,
+                  )
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: InkWell(
+              onTap: () {
+                provider.changeLang('ar');
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    AppLocalizations.of(context)!.arabic,
+                    style: TextStyle(fontSize: 30, color: Colors.green),
+                  ),
+                  Icon(
+                    Icons.check,
+                    size: 30,
+                    color: Colors.green,
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      );
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,22 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:news_project/home/home_screen.dart';
 import 'package:news_project/home/search_screen.dart';
 import 'package:news_project/news/news_details_screen.dart';
+import 'package:news_project/providers/app_config.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ChangeNotifierProvider(
+      create: (BuildContext context) =>
+        AppConfigProvider()
+      ,
+      child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    var provider = Provider.of<AppConfigProvider>(context);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       routes: {
@@ -22,7 +29,7 @@ class MyApp extends StatelessWidget {
       initialRoute: HomeScreen.RouteName,
       supportedLocales: AppLocalizations.supportedLocales,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
-      locale: Locale('ar'),
+      locale: provider.language == 'en' ? Locale('en') : Locale('ar'),
     );
   }
 }
